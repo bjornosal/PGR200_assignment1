95ef6c548f31df863d50da260155dd1ad843bcd7
bjornosal <bjorn.olav.salvesen@gmail.com>
2017-09-26 13:18:23 +0200

    Added minor functionality for reading input from user for file-picking

--- src/main/java/no/salvesen/assignment1/App.java
+++ src/main/java/no/salvesen/assignment1/App.java
@@ -5,7 +5,9 @@ import java.sql.SQLException;
 public class App
 {
     public static void main(String[] args) throws SQLException {
-        DatabaseConnector dbc = new DatabaseConnector();
-        dbc.databaseBuilder();
+       // DatabaseConnector dbc = new DatabaseConnector();
+     //   dbc.databaseBuilder();
+        InputHandler inph = new InputHandler();
+        inph.fileFinder();
     }
 }
--- src/main/java/no/salvesen/assignment1/InputHandler.java
+++ src/main/java/no/salvesen/assignment1/InputHandler.java
@@ -1,5 +1,7 @@
 package no.salvesen.assignment1;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -8,18 +10,52 @@ public class InputHandler {
 
     //TODO Method for checking that file is correct format
     //TODO Method for parsing file
+    private File subjectFile;
+    private File roomFile;
+    private File lecturerFile;
+    private int tablePick;
+    private String tableName;
+    private String filePath;
 
-    public void inputReader() {
-        try {
-            Scanner input = new Scanner(new File("C:/IdeaProjects/assignment1/src/files"));
-            input.useDelimiter(";");
+    public void fileFinder() {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please choose which table you are entering information for: ");
+        System.out.println("1: Subject");
+        System.out.println("2: Room");
+        System.out.println("3: Lecturer");
+        tablePick = userInput.nextInt();
 
-            while(input.hasNext()) {
-                System.out.println();
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        switch(tablePick) {
+            case 1:
+                tableName = "subject";
+                break;
+            case 2:
+                tableName = "room";
+                break;
+            case 3:
+                tableName = "lecturer";
+                break;
+            default:
+                tableName = "subject";
+                break;
+
         }
+        System.out.println("Please copy the file-path to the csv file for " + tableName + "s.");
+        userInput.nextLine();
+        //TODO: Test required to check that file is actually a file
+        filePath = userInput.nextLine();
+        switch(tablePick) {
+            case 1:
+                subjectFile = new File(filePath);
+                break;
+            case 2:
+                roomFile = new File(filePath);
+                break;
+            case 3:
+                lecturerFile = new File(filePath);
+                break;
+        }
+        System.out.println(filePath);
     }
 
 
